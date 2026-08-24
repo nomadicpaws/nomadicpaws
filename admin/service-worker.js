b40077fd6fa3f9c915b3e71f392e78c8c7eaa8a3
nomadicpaws <breakthroughmiraclepower@gmail.com>
2026-08-23 20:20:48 -0700

    Make mobile story options fully auto height

--- admin/service-worker.js
+++ admin/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'nomadic-paws-cms-v6';
-const SHELL = ['/admin/', '/admin/admin.css?v=6', '/admin/app.js?v=5', '/admin/pinterest-preview.js?v=5', '/admin/review-utils.js', '/admin/photo-utils.js', '/admin/manifest.webmanifest', '/apple-touch-icon.png'];
+const CACHE = 'nomadic-paws-cms-v7';
+const SHELL = ['/admin/', '/admin/admin.css?v=7', '/admin/app.js?v=5', '/admin/pinterest-preview.js?v=5', '/admin/review-utils.js', '/admin/photo-utils.js', '/admin/manifest.webmanifest', '/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
